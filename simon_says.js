3e2767fedb20e2374e5a2fe16de94fc88ae2fe66
Mark Jarvis <mtgjarvis@gmail.com>
2019-09-04 15:13:33 -0400

    completed writing code to pass tests

--- simon_says.js
+++ simon_says.js
@@ -1,1 +1,27 @@
+
+const echo = string => {
+    return string;
+};
+
+const shout = string => {
+    return string.toUpperCase();
+};
+
+const repeat = (string, num) => {
+    addSpace = string.concat(' ')
+    newString = addSpace.repeat(num);
+    newString = newString.substring(0, newString.length - 1);
+    return newString;
+};
+
+const startOfWord = (string, num) => {
+    newString = string.slice(0, num);
+    return newString;
+}
+
+const firstWord = (string, num) => {
+    newString = string.split(' ');
+    return newString[0]
+}
+
 module.exports = {echo: echo, shout: shout, repeat: repeat, startOfWord: startOfWord, firstWord: firstWord};
